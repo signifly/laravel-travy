d031506e154c95e97ee043ac5a1ac5a6a102c172
Morten Poul Jensen <mpj@signifly.com>
2018-10-10 14:26:03 +0200

    Hide editor from index

---
+++ src/Fields/Editor.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Signifly\Travy\Fields;
+
+use Signifly\Travy\FieldTypes\FieldType;
+
+class Editor extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'editor';
+
+    /**
+     * Indicates if the element should be shown on the index view.
+     *
+     * @var bool
+     */
+    public $showOnIndex = false;
+
+    /**
+     * The options to apply to the field type.
+     *
+     * @param  FieldType $fieldType
+     * @return void
+     */
+    public function options(FieldType $fieldType)
+    {
+        $fieldType->content($this->attribute);
+    }
+}
