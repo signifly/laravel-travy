f10de23e28dbde08d685c2ae9d1a6fc07ab74750
Morten Poul Jensen <mpj@signifly.com>
2018-10-11 11:41:10 +0200

    Cache definitions response for 1 hour

--- src/Http/Controllers/DefinitionController.php
+++ src/Http/Controllers/DefinitionController.php
@@ -4,6 +4,7 @@ namespace Signifly\Travy\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Cache;
 use Signifly\Travy\Support\DefinitionFactory;
 use Signifly\Travy\Http\Requests\TravyRequest;
 
@@ -13,6 +14,24 @@ class DefinitionController extends Controller
     {
         $definition = (new DefinitionFactory($request))->make();
 
-        return new JsonResponse($definition->build());
+        $cacheKey = $this->getCacheKeyFor($definition);
+        $cacheTtl = 60; // minutes
+
+        return Cache::remember($cacheKey, $cacheTtl, function () use ($definition) {
+            return new JsonResponse($definition->build());
+        });
+    }
+
+    /**
+     * Get the cache key for the definition.
+     *
+     * @param  \Signifly\Travy\Schema\Definition $definition
+     * @return string
+     */
+    protected function getCacheKeyFor($definition)
+    {
+        $class = get_class($definition);
+
+        return md5($class);
     }
 }
