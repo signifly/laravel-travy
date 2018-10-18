4cb6a55b5687b67fafc8732998e470e3600ccad9
Morten Poul Jensen <mpj@signifly.com>
2018-10-18 12:01:59 +0200

    Set defaultValue to 0

--- src/Fields/InputNumber.php
+++ src/Fields/InputNumber.php
@@ -14,6 +14,13 @@ class InputNumber extends Field
     public $component = 'inputNumber';
 
     /**
+     * The default value for the field.
+     *
+     * @var mixed
+     */
+    public $defaultValue = 0;
+
+    /**
      * The options to apply to the field type.
      *
      * @param  FieldType $fieldType
