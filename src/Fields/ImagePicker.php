a08c9f9578e0cccb6623c5b35645b2d715baf36a
Morten Poul Jensen <mpj@signifly.com>
2018-11-07 15:10:53 +0100

    Add ImagePicker field

---
+++ src/Fields/ImagePicker.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Signifly\Travy\Fields;
+
+use Signifly\Travy\FieldTypes\FieldType;
+
+class ImagePicker extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'imagePicker';
+
+    public function options(array $options)
+    {
+        return $this->withMeta(['options' => $options]);
+    }
+
+    public function url(string $key)
+    {
+        return $this->withMeta(['url' => $key]);
+    }
+
+    /**
+     * The options to apply to the field type.
+     *
+     * @param  FieldType $fieldType
+     * @return void
+     */
+    public function applyOptions(FieldType $fieldType)
+    {
+        $fieldType->id($this->attribute);
+    }
+}
