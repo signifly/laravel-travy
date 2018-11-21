db389b4de00454076fc4768e50d16b2013626940
Morten Poul Jensen <mpj@signifly.com>
2018-11-21 13:04:53 +0100

    Add actions column to default table

--- src/Schema/DefaultTableDefinition.php
+++ src/Schema/DefaultTableDefinition.php
@@ -2,6 +2,10 @@
 
 namespace Signifly\Travy\Schema;
 
+use Signifly\Travy\Schema\Action;
+use Signifly\Travy\Schema\Column;
+use Signifly\Travy\Fields\Actions;
+
 class DefaultTableDefinition extends TableDefinition
 {
     protected function schema()
@@ -17,5 +21,22 @@ class DefaultTableDefinition extends TableDefinition
 
         // Set columns, filters, includes and modifiers from resource
         $this->loadFromResource();
+
+        // Set actions
+        $column = Column::make(
+            Actions::make('Actions')
+                ->actions([
+                    Action::make('View')
+                        ->type('show')
+                        ->endpoint("/t/{$this->getResourceKey()}/{id}", 'get'),
+
+                    Action::make('Delete')
+                        ->type('popup')
+                        ->endpoint(url("v1/admin/{$this->getResourceKey()}/{id}"), 'delete')
+                        ->text('Are you sure? Please confirm this action.')
+                ])
+                ->width(120)
+        );
+        $this->addColumn($column);
     }
 }
