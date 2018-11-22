d39bff64584709a31aa084e0bd1ed0b677fbe60c
Morten Poul Jensen <mpj@signifly.com>
2018-11-22 15:38:44 +0100

    Add delete action to default view

--- src/Schema/DefaultViewDefinition.php
+++ src/Schema/DefaultViewDefinition.php
@@ -2,10 +2,21 @@
 
 namespace Signifly\Travy\Schema;
 
+use Signifly\Travy\Schema\Action;
+
 class DefaultViewDefinition extends ViewDefinition
 {
     protected function schema()
     {
         $this->loadFromResource();
+
+        $this->actions([
+            Action::make('Delete', 'danger')
+                ->icon('delete')
+                ->type('popup')
+                ->endpoint(url("v1/admin/{$this->getResourceKey()}/{id}"), 'delete')
+                ->onSubmit("/t/{$this->getResourceKey()}")
+                ->text('Are you sure? Please confirm the action.'),
+        ]);
     }
 }
