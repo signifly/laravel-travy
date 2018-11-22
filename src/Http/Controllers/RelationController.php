6f73bbc0ca38a5c3f77b6ab1518521f833b529a0
Morten Poul Jensen <mpj@signifly.com>
2018-11-22 10:58:33 +0100

    Fix relation controller

--- src/Http/Controllers/RelationController.php
+++ src/Http/Controllers/RelationController.php
@@ -3,15 +3,17 @@
 namespace Signifly\Travy\Http\Controllers;
 
 use Spatie\QueryBuilder\QueryBuilder;
+use Illuminate\Database\Eloquent\Model;
 use Signifly\Travy\Http\Actions\IndexAction;
 use Signifly\Travy\Http\Requests\TravyRequest;
+use Illuminate\Database\Eloquent\Relations\Relation;
 
 class RelationController extends Controller
 {
     public function index(TravyRequest $request)
     {
         $relationName = $request->relationName();
-        $model = $request->resource()->findOrFail($request->id);
+        $model = $request->resource()->findOrFail($request->resourceId());
 
         $this->guardAgainstInvalidRelation($model, $relationName);
 
@@ -44,7 +46,7 @@ class RelationController extends Controller
     public function show(TravyRequest $request)
     {
         $relationName = $request->relationName();
-        $model = $request->resource()->findOrFail($request->id);
+        $model = $request->resource()->findOrFail($request->resourceId());
 
         $this->guardAgainstInvalidRelation($model, $relationName);
 
@@ -66,10 +68,36 @@ class RelationController extends Controller
     protected function guardAgainstInvalidRelation($model, $relationName)
     {
         abort_unless(
-            $model->hasRelation($relationName),
+            $this->hasRelation($model, $relationName),
             404,
             'Not Found.'
         );
+    }
+
+    /**
+     * Checks if a model has a given relation.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model $model
+     * @param  string $relationName
+     * @return bool
+     */
+    protected function hasRelation(Model $model, string $relationName)
+    {
+        // If the key already exists in the relationships array, it just means the
+        // relationship has already been loaded, so we'll just return it out of
+        // here because there is no need to query within the relations twice.
+        if ($model->relationLoaded($relationName)) {
+            return true;
+        }
+
+        // If the "attribute" exists as a method on the model, we will just assume
+        // it is a relationship and will load and return results from the query
+        // and hydrate the relationship's value on the "relationships" array.
+        if (method_exists($model, $relationName)) {
+            return is_a($model->$relationName(), Relation::class);
+        }
+
+        return false;
     }
 
     /**
