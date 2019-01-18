c24cea3005fea0f61c9cc989057147666e4a79c8
Morten Poul Jensen <mpj@signifly.com>
2019-01-18 14:00:08 +0100

    Add precision field

---
+++ src/Fields/Input/Precision.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Signifly\Travy\Fields\Input;
+
+use Signifly\Travy\Fields\Field;
+
+class Precision extends Field
+{
+    /**
+     * The field's component.
+     *
+     * @var string
+     */
+    public $component = 'input-precision';
+
+    /**
+     * The default value for the field.
+     *
+     * @var mixed
+     */
+    public $defaultValue = 0;
+
+    public function max(int $value)
+    {
+        return $this->withProps(['max' => $value]);
+    }
+
+    public function precision(int $value)
+    {
+        return $this->withProps(['precision' => $value]);
+    }
+
+    public function step(int $value)
+    {
+        return $this->withProps(['step' => $value]);
+    }
+
+    /**
+     * The options to apply to the field type.
+     *
+     * @return void
+     */
+    public function applyOptions()
+    {
+        $this->withProps(['value' => $this->attribute]);
+    }
+}
