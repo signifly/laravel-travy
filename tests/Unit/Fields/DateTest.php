23ad276c767047391e13c16956ea64745ecc5af9
Morten Poul Jensen <mpj@signifly.com>
2019-01-22 16:49:01 +0100

    Add basic unit test

---
+++ tests/Unit/Fields/DateTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Signifly\Travy\Test\Unit\Fields;
+
+use Signifly\Travy\Fields\Date;
+use Signifly\Travy\Test\TestCase;
+
+class DateTest extends TestCase
+{
+    /** @test */
+    function is_serializes_to_json()
+    {
+        $date = Date::make('Created at');
+
+        $expected = [
+            'name' => 'created_at',
+            'label' => 'Created at',
+            'fieldType' => [
+                'id' => 'date',
+                'props' => [
+                    'timestamp' => 'created_at',
+                ],
+            ]
+        ];
+        $this->assertEquals($expected, $date->jsonSerialize());
+    }
+
+    /** @test */
+    function it_allows_to_set_width()
+    {
+        $date = Date::make('Created at')->width(50);
+
+        $this->assertEquals(50, $date->width->getValue());
+    }
+}
