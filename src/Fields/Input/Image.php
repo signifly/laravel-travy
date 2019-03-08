f2a57813ca4ac855df9fb59a9c6aafeac8ab5dee
Morten Poul Jensen <mpj@signifly.com>
2019-03-08 15:20:29 +0100

    Add Image field helper methods

--- src/Fields/Input/Image.php
+++ src/Fields/Input/Image.php
@@ -20,9 +20,29 @@ class Image extends Field
      */
     public $showOnIndex = false;
 
+    public function download(bool $value)
+    {
+        return $this->withProps(['download' => $value]);
+    }
+
+    public function height(string $val)
+    {
+        return $this->withProps(['height' => $value]);
+    }
+
+    public function upload(bool $value)
+    {
+        return $this->withProps(['upload' => $value]);
+    }
+
     public function url(string $key)
     {
         return $this->withProps(['url' => $key]);
+    }
+
+    public function width(string $value)
+    {
+        return $this->withProps(['width' => $value]);
     }
 
     /**
