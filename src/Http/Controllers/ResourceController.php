6c30f1f7025a2d33e7d1d33a051e2e09859ad9cd
Morten Poul Jensen <mpj@signifly.com>
2019-03-12 11:15:53 +0100

    Pass request object to methods

--- src/Http/Controllers/ResourceController.php
+++ src/Http/Controllers/ResourceController.php
@@ -13,42 +13,42 @@ class ResourceController extends Controller
 
     public function store(TravyRequest $request)
     {
-        $request->resource()->authorizeToCreate();
+        $request->resource()->authorizeToCreate($request);
 
         return $this->dispatch($request->action());
     }
 
     public function show(TravyRequest $request)
     {
-        $request->resource()->authorizeToView();
+        $request->resource()->authorizeToView($request);
 
         return $this->dispatch($request->action());
     }
 
     public function update(TravyRequest $request)
     {
-        $request->resource()->authorizeToUpdate();
+        $request->resource()->authorizeToUpdate($request);
 
         return $this->dispatch($request->action());
     }
 
     public function destroy(TravyRequest $request)
     {
-        $request->resource()->authorizeToDelete();
+        $request->resource()->authorizeToDelete($request);
 
         return $this->dispatch($request->action());
     }
 
     public function restore(TravyRequest $request)
     {
-        $request->resource()->authorizeToRestore();
+        $request->resource()->authorizeToRestore($request);
 
         return $this->dispatch($request->action());
     }
 
     public function forceDestroy(TravyRequest $request)
     {
-        $request->resource()->authorizeToForceDelete();
+        $request->resource()->authorizeToForceDelete($request);
 
         return $this->dispatch($request->action());
     }
