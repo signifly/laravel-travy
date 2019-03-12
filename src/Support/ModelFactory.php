8bb0ccae57140d680d10b89da2480b42b72d36cf
Morten Poul Jensen <mpj@signifly.com>
2019-03-12 13:34:58 +0100

    Update ModelFactory

--- src/Support/ModelFactory.php
+++ src/Support/ModelFactory.php
@@ -12,8 +12,33 @@ class ModelFactory
         $reflectionInstance = $reflection->newInstanceWithoutConstructor();
         $modelClass = $reflectionInstance->modelClass() ?? $resourceClass::guessModel();
 
-        return ! is_null($resourceId)
-            ? $modelClass::find($resourceId)
-            : new $modelClass();
+        abort_unless(
+            class_exists($modelClass),
+            404,
+            'Not Found.'
+        );
+
+        if (is_null($resourceId)) {
+            return new $modelClass();
+        }
+
+        if (self::softDeletes($modelClass)) {
+            return $modelClass::withTrashed()->findOrFail($resourceId);
+        }
+
+        return $modelClass::findOrFail($resourceId);
+    }
+
+    /**
+     * Determine if a model uses soft deletes.
+     *
+     * @return bool
+     */
+    public static function softDeletes(string $modelClass)
+    {
+        return in_array(
+            SoftDeletes::class,
+            class_uses_recursive($modelClass)
+        );
     }
 }
