9ae579dd79d127e82f7af299b0d4dc4243c897b2
Morten Poul Jensen <mpj@signifly.com>
2019-04-09 14:58:18 +0200

    Add width to section

--- src/Fields/Section.php
+++ src/Fields/Section.php
@@ -4,4 +4,31 @@ namespace Signifly\Travy\Fields;
 
 class Section extends Tab
 {
+    /** @var int */
+    protected $width = 50;
+
+    /**
+     * Set the width on the section.
+     *
+     * @param  int    $value
+     * @return self
+     */
+    public function width(int $value): self
+    {
+        $this->width = $value;
+
+        return $this;
+    }
+
+    /**
+     * Prepare the sidebar for JSON serialization.
+     *
+     * @return array
+     */
+    public function jsonSerialize()
+    {
+        return array_merge(parent::jsonSerialize(), [
+            'width' => $this->width,
+        ]);
+    }
 }
