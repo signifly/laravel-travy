61c076f309407d0729f7658c4fb7686c3dc85344
Morten Poul Jensen <mpj@signifly.com>
2019-06-24 09:35:29 +0200

    Add TabTest

---
+++ tests/Unit/Fields/TabTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Signifly\Travy\Test\Unit\Fields;
+
+use Signifly\Travy\Fields\Tab;
+use Signifly\Travy\Fields\Date;
+use Signifly\Travy\Test\TestCase;
+
+class TabTest extends TestCase
+{
+    /** @test */
+    public function it_serializes_to_json()
+    {
+        $field = Tab::make('Details')
+            ->endpoint(url('v1/admin/products/{id}'))
+            ->fields([
+                Date::make('Created at'),
+            ]);
+
+        $expected = [
+            'id' => 'details',
+            'type' => 'fields',
+            'endpoint' => ['url' => 'http://localhost/v1/admin/products/{id}'],
+            'title' => ['text' => 'Details', 'url' => ''],
+            'fields' => [
+                [
+                    'name' => 'created_at',
+                    'label' => 'Created at',
+                    'fieldType' => [
+                        'id' => 'date',
+                        'props' => [
+                            'timestamp' => 'created_at',
+                        ],
+                    ]
+                ],
+            ],
+        ];
+        $this->assertEquals($expected, $field->jsonSerialize());
+    }
+}
