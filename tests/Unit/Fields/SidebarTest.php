04c2ffbba2d0bf91c92e0a88b5da684b11c3ec67
Morten Poul Jensen <mpj@signifly.com>
2019-06-24 09:25:09 +0200

    Add SidebarTest

---
+++ tests/Unit/Fields/SidebarTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Signifly\Travy\Test\Unit\Fields;
+
+use Signifly\Travy\Fields\Date;
+use Signifly\Travy\Test\TestCase;
+use Signifly\Travy\Fields\Sidebar;
+
+class SidebarTest extends TestCase
+{
+    /** @test */
+    public function it_serializes_to_json()
+    {
+        $field = Sidebar::make('History')
+            ->fields([
+                Date::make('Created at'),
+            ]);
+
+        $expected = [
+            'id' => 'history',
+            'title' => 'History',
+            'fields' => [
+                [
+                    'name' => 'created_at',
+                    'label' => 'Created at',
+                    'fieldType' => [
+                        'id' => 'date',
+                        'props' => [
+                            'timestamp' => 'created_at',
+                        ],
+                    ]
+                ],
+            ],
+        ];
+        $this->assertEquals($expected, $field->jsonSerialize());
+    }
+}
