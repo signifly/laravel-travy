c6deb262a6b38da0119551b35495e06f5a51c8d5
Morten Poul Jensen <mpj@signifly.com>
2019-06-24 08:58:15 +0200

    Remove authorize method as the FormRequest defaults to true

--- src/Http/Requests/TravyRequest.php
+++ src/Http/Requests/TravyRequest.php
@@ -19,16 +19,6 @@ class TravyRequest extends FormRequest
     protected $relationResource;
 
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
