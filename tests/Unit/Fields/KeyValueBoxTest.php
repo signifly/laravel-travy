4e86ff0718ea44eaa0a0a055d94d2236af321028
Morten Poul Jensen <mpj@signifly.com>
2019-06-25 09:28:51 +0200

    Add KeyValueBoxTest

---
+++ tests/Unit/Fields/KeyValueBoxTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Signifly\Travy\Test\Unit\Fields;
+
+use Signifly\Travy\Test\TestCase;
+use Signifly\Travy\Fields\KeyValueBox;
+
+class KeyValueBoxTest extends TestCase
+{
+    /** @test */
+    public function it_serializes_to_json()
+    {
+        $field = KeyValueBox::make('Some title')
+            ->addItem('Name', 'name')
+            ->addItem('Items', 'items_count');
+
+        $expected = [
+            'name' => 'some_title',
+            'label' => 'Some title',
+            'fieldType' => [
+                'id' => 'key-value-box',
+                'props' => [
+                    'items' => [
+                        ['label' => 'Name', 'value' => 'name'],
+                        ['label' => 'Items', 'value' => 'items_count'],
+                    ],
+                ],
+            ],
+        ];
+        $this->assertEquals($expected, $field->jsonSerialize());
+    }
+}
