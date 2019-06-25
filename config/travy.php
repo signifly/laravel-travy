637d6bb61069b00d05b64b1cf3fd60b0e1c00425
Morten Poul Jensen <mpj@signifly.com>
2019-06-25 09:31:26 +0200

    Add comments to config

--- config/travy.php
+++ config/travy.php
@@ -1,23 +1,40 @@
 <?php
 
 return [
+
+    /*
+     * The namespace to use when generating actions.
+     */
     'actions' => [
         'namespace' => 'App\\Travy\\Http\\Actions',
     ],
 
+    /*
+     * The namespace to use when generating table and view definitions.
+     */
     'definitions' => [
         'namespace' => 'App\\Travy\\Definitions',
     ],
 
+    /*
+     * The namespace to use when generating fields.
+     */
     'fields' => [
         'namespace' => 'App\\Travy\\Fields',
     ],
 
+    /*
+     * The namespace to use when generating models.
+     */
     'models' => [
         'namespace' => 'App\\Models',
     ],
 
+    /*
+     * The namespace to use when generating resources.
+     */
     'resources' => [
         'namespace' => 'App\\Travy',
     ],
+
 ];
