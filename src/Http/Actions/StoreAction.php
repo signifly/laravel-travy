2baf04fd264d388b9a0833cdf7bae12345510533
Morten Poul Jensen <mpj@foreno.dk>
2019-07-03 19:16:38 +0200

    Wrap logic in a database transaction

--- src/Http/Actions/StoreAction.php
+++ src/Http/Actions/StoreAction.php
@@ -2,15 +2,27 @@
 
 namespace Signifly\Travy\Http\Actions;
 
+use Exception;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\Support\Responsable;
 
 class StoreAction extends Action
 {
     public function handle(): Responsable
     {
-        $model = $this->resource->create($this->input->all());
+        try {
+            DB::beginTransaction();
 
-        $this->syncRelations();
+            $model = $this->resource->create($this->input->all());
+
+            $this->syncRelations();
+
+            DB::commit();
+        } catch (Exception $exception) {
+            DB::rollBack();
+
+            throw $exception;
+        }
 
         return $this->respond(
             $model->load($this->resource->with())
--- src/Http/Actions/UpdateAction.php
+++ src/Http/Actions/UpdateAction.php
@@ -2,17 +2,29 @@
 
 namespace Signifly\Travy\Http\Actions;
 
+use Exception;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\Support\Responsable;
 
 class UpdateAction extends Action
 {
     public function handle(): Responsable
     {
-        $model = $this->resource->model();
+        try {
+            DB::beginTransaction();
 
-        $model->update($this->input->all());
+            $model = $this->resource->model();
 
-        $this->syncRelations();
+            $model->update($this->input->all());
+
+            $this->syncRelations();
+
+            DB::commit();
+        } catch (Exception $exception) {
+            DB::rollBack();
+
+            throw $exception;
+        }
 
         return $this->respond(
             $model->fresh($this->resource->with())
