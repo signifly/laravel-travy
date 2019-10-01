006d145e32c7e96d260c7494d9ecf52af16fa411
Travis Elkins <travis.elkins@gmail.com>
2019-10-01 19:26:22 +0200

    Tabs -> spaces

--- src/Exceptions/InvalidPropsException.php
+++ src/Exceptions/InvalidPropsException.php
@@ -7,12 +7,12 @@ use Illuminate\Contracts\Support\MessageBag;
 
 class InvalidPropsException extends Exception
 {
-	public $errors;
+    public $errors;
 
-	public function __construct(MessageBag $errors, $message = null, $code = 0, Exception $previous = null)
-	{
-		$this->errors = $errors;
+    public function __construct(MessageBag $errors, $message = null, $code = 0, Exception $previous = null)
+    {
+        $this->errors = $errors;
 
-		parent::__construct($message, $code, $previous);
-	}
+        parent::__construct($message, $code, $previous);
+    }
 }
